5b3e12415e6815a3d0317f7deca583c92a28e763
qkrrlgus114 <qkrrlgus114@naver.com>
2024-06-13 15:50:22 +0900

    Feat : 스레드 생성 실습

---
+++ Java-Concurrency/src/chapter02/exam01/AnonymousRunnableClassExample.java
@@ -0,0 +1,15 @@
+package chapter02.exam01;
+
+public class AnonymousRunnableClassExample {
+    public static void main(String[] args) {
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println(Thread.currentThread().getName() + ": 스레드 실행중...");
+            }
+        });
+
+        thread.run();
+    }
+}
---
+++ Java-Concurrency/src/chapter02/exam01/ImplementRunnableExample.java
@@ -0,0 +1,19 @@
+package chapter02.exam01;
+
+public class ImplementRunnableExample {
+    public static void main(String[] args) {
+
+        MyRunnable task = new MyRunnable();
+        Thread thread = new Thread(task);
+
+        thread.run();
+    }
+}
+
+class MyRunnable implements Runnable{
+
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName() + ": 스레드 실행중");
+    }
+}

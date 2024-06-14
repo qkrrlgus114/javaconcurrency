da7795a7ba023694425ef0512b4284cbd99fcff2
qkrrlgus114 <qkrrlgus114@naver.com>
2024-06-14 22:03:51 +0900

    Feat : 뮤텍스 상호배제 실습

---
+++ Java-Concurrency/src/chapter06/Mutex.java
@@ -0,0 +1,21 @@
+package chapter06;
+
+public class Mutex {
+    private boolean lock = false;
+
+    public synchronized void acquired() {
+        while (lock) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        this.lock = true;
+    }
+
+    public synchronized void release() {
+        this.lock = false;
+        this.notify();
+    }
+}
